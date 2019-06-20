48dadd43ada774164a88ea14a20c934b9b20dd48
Guillermo Varela <42847218+guillo16@users.noreply.github.com>
2019-06-20 12:19:52 -0300

    Revert "Style"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :contacts, only: [:index]
-  resources :english, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20190620151218_create_englishes.rb
+++
@@ -1,9 +0,0 @@
-class CreateEnglishes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :englishes do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
